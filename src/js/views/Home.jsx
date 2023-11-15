61b7224d3a6c837b2c7dbdef9de2959176cd8228
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2023-11-15 19:57:10 +0000

    feat: title app

--- src/js/views/Home.jsx
+++ src/js/views/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+		<h1>React App</h1>
 		<p>
 			<img src={rigoImage} />
 		</p>
